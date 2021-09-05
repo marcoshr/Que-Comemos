436ddcb7b9e7f2f295c380fd7bbc88da7e7118c1
Marcos Hernández Rodríguez <marcoshernandezhr@gmail.com>
2021-09-06 00:13:05 +0100

    .

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ function handleClickSearch() {
     console.log("qwerty")
 
     // const query = d3.select('#boton').property("value");
-    document.getElementById("content").innerHTML = "prueba";
+    document.getElementById("content").innerHTML = comidas;
+    document.getElementById("content2").innerHTML = "pepe";
 
 } 
