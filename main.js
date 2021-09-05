ee8578f5c4f088aa6b737e5aad3542ac5be6af94
Marcos Hernández Rodríguez <marcoshernandezhr@gmail.com>
2021-09-05 23:46:52 +0100

    .

---
+++ main.js
@@ -0,0 +1,7 @@
+var comidas = ["1", "2", "3"]
+
+function handleClickSearch() {
+
+    // const query = d3.select('#boton').property("value");
+    document.getElementById("content").innerHTML = "prueba";
+}
