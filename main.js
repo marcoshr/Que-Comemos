0e1afd5c6fa5784c7060234829dcf281cbc13d0f
Marcos Hernández Rodríguez <marcoshernandezhr@gmail.com>
2021-09-06 00:16:11 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 console.log("console3")
-var comidas = ["1", "2", "3"]
+var comidas = ["1", "2", "3", ]
 
 function handleClickSearch() {
     console.log("qwerty")
 
+    rand = Math.floor((Math.random() * comidas.length) + 1)
+
     // const query = d3.select('#boton').property("value");
-    document.getElementById("content").innerHTML = comidas;
+    document.getElementById("content").innerHTML = comidas[rand];
     document.getElementById("content2").innerHTML = "pepe";
 
 } 
