4404ac6b53db651d55f986fe238e17598d16e865
Marcos Hernández Rodríguez <marcoshernandezhr@gmail.com>
2021-09-06 00:10:10 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ console.log("console3")
 var comidas = ["1", "2", "3"]
 
 function handleClickSearch() {
+    console.log("qwerty")
 
     // const query = d3.select('#boton').property("value");
     document.getElementById("content").innerHTML = "prueba";
